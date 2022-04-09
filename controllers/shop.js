d84c5ff68e536e314ecc7b6286660ca72613701f
Michael Bruno <brunomike254@gmail.com>
2022-04-09 13:06:54 +0300

    Finished updating the app to use vanilla mongodb

--- controllers/shop.js
+++ controllers/shop.js
@@ -46,51 +46,31 @@ exports.getIndex = (req, res, next) => {
     }).catch((err) => console.log(err.message));
 };
 
-// exports.postOrder = (req, res, next) => {
-//   let fetchedCart;
-//   req.user.getCart()
-//     .then((cart) => {
-//       fetchedCart = cart;
-//       return cart.getProducts()
-//     })
-//     .then(products => {
-//       return req.user.createOrder()
-//         .then(order => {
-//           return order.addProducts(
-//             products.map(product => {
-//               product.orderItem = { quantity: product.cartItem.quantity };
-//               return product;
-//             }))
-//         })
-//         .catch(err => console.log(err.message));
-//     })
-//     .then(result => {
-//       return fetchedCart.setProducts(null);
-//     })
-//     .then(result => {
-//       res.redirect('/orders')
-//     })
-//     .catch((err) => console.log(err.message))
-// };
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user.addOrder()
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch((err) => console.log(err.message));
+};
 
-// exports.getOrders = (req, res, next) => {
-//   req.user.getOrders({ include: ['products'] })
-//     .then(orders => {
-//       //console.log(orders);
-//       res.render("shop/orders", {
-//         docTitle: "My Orders",
-//         path: "/orders",
-//         orders: orders
-//       });
-//     })
-//     .catch(err => console.log(err.message))
-
-// };
+exports.getOrders = (req, res, next) => {
+  req.user.getOrders()
+    .then(orders => {
+      res.render("shop/orders", {
+        docTitle: "My Orders",
+        path: "/orders",
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err.message));
+};
 
 
 exports.getCart = (req, res, next) => {
   req.user.getCart()
-    .then(products => {      
+    .then(products => {
       res.render("shop/cart", {
         docTitle: "My Cart",
         path: "/cart",
@@ -112,68 +92,17 @@ exports.postCart = (req, res, next) => {
       res.redirect('/cart');
     }).catch(err => console.log(err.message));
 
-
-
-  // let fetchedCart;
-  // let newQuantity = 1;
-
-  // req.user.getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: productId } })
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findOne({ where: { id: productId } })
-  //     // return Product.findOne({ where: { id: productId } })
-  //     //   .then(selectedProduct => {
-  //     //     return fetchedCart.addProduct(selectedProduct, {
-  //     //       through: { quantity: newQuantity }
-  //     //     });
-
-  //     //   }).catch(err => console.log(err.message))
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     })
-  //   })
-  //   .then(() => {
-  //     res.redirect('/cart');
-  //   })
-  //   .catch(err => console.log(err.message))
-
-  // // Product.findById(productId.trim(), product => {
-  // //   Cart.addProduct(productId.trim(), product.price);
-  // // });
-
 };
 
 
-// exports.postCartDeleteProduct = (req, res, next) => {
-//   const productId = req.body.productId.trim();
-//   req.user.getCart()
-//     .then((cart) => {
-//       return cart.getProducts({ where: { id: productId } })
-//     })
-//     .then(products => {
-//       const product = products[0];
-//       return product.cartItem.destroy()
-//     })
-//     .then(result => {
-//       res.redirect('/cart')
-//     })
-//     .catch(err => console.log(err.message))
-// };
+exports.postCartDeleteProduct = (req, res, next) => {
+  const productId = req.body.productId.trim();
+  req.user.deleteItemFromCart(productId)
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err.message));
+};
 
 // exports.getCheckout = (req, res, next) => {
 //   res.render("shop/chekout", { docTitle: "Checkout", path: "/checkout" });
--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const { get } = require('express/lib/response');
 const mongodb = require('mongodb');
 const getDb = require("../utils/database").getDb;
 
@@ -49,10 +50,45 @@ class User {
                 };
             });
         }).catch(err => console.log(err.message));
-
-        //return this.cart;
     }
 
+    deleteItemFromCart(productId) {
+        const updatedCartItems = this.cart.items.filter(item => {
+            return item.productId.toString() !== productId.toString();
+        });
+        const db = getDb();
+        return db.collection('users').updateOne(
+            { _id: new mongodb.ObjectId(this._id) },
+            { $set: { cart: { items: updatedCartItems } } }
+        );
+    }
+
+    addOrder() {
+        const db = getDb();
+        return this.getCart().then(products => {
+            const order = {
+                items: products,
+                user: {
+                    _id: new mongodb.ObjectId(this._id),
+                    name: this.name,
+                    email: this.email
+                }
+            };
+            return db.collection('orders').insertOne(order);
+        }).then(result => {
+            this.cart = { items: [] };
+            return db.collection('users').updateOne(
+                { _id: new mongodb.ObjectId(this._id) },
+                { $set: { cart: { items: [] } } }
+            );
+        }).catch(err => console.log(err.message));
+
+    }
+
+    getOrders() {
+        const db = getDb();
+        return db.collection('orders').find({ "user._id": new mongodb.ObjectId(this._id) }).toArray();   
+    }
 
     static findById(userId) {
         const db = getDb();
